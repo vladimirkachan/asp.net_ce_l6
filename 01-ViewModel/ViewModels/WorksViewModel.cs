e66b6f91a2d14fd3afb8e866cd0930314d3afdb9
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-18 19:32:06 +0300

    ViewModel

---
+++ 01-ViewModel/ViewModels/WorksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_ViewModel.Models;
+
+namespace _01_ViewModel.ViewModels
+{
+    public class WorksViewModel
+    {
+        public IEnumerable<Person> Persons {get; set;}
+        public IEnumerable<Company> Companies {get; set;}
+    }
+}
