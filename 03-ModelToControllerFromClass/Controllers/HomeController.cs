115c1e151ea391da30a57e0a580c7f3d2fe2edfa
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-18 20:59:23 +0300

    ModelToControllerFromClass

---
+++ 03-ModelToControllerFromClass/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using _03_ModelToControllerFromClass.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _03_ModelToControllerFromClass.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string ProceedResult(User user)
+        {
+            return $"{user.FirstName} {user.Surname} - {user.Age} years old";
+        }
+    }
+}
